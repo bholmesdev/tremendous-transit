790b2fbe724210622c0f33ef2378d443bbdb0a6e
bholmesdev <hey@bholmes.dev>
2024-07-30 11:36:43 -0400

    feat: add logout test

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,8 +4,28 @@ export const server = {
   test: defineAction({
     input: z.object({key: z.string()}),
     async handler(input) {
-      await new Promise((res) => setTimeout(res, 3000))
+      await new Promise((res) => setTimeout(res, 300))
       return input
     }
+  }),
+  logoutUser: defineAction({
+    handler: (_, context) => {
+      const TOKEN = import.meta.env.TOKEN
+  
+      try {
+        context.cookies.set(TOKEN, '', {
+          httpOnly: true,
+          maxAge: 0,
+          path: '/',
+          secure: true,
+          sameSite: 'lax',
+        })
+  
+        return { cookieDeleted: true }
+      } catch (error) {
+        console.error('Error logging out: ', error)
+        throw new Error(error.message || error)
+      }
+    },
   })
 }
