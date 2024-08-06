a316e730173f8a24d2b767b830e14706051a29ed
bholmesdev <hey@bholmes.dev>
2024-08-06 08:00:40 -0400

    deps: astro latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import netlify from "@astrojs/netlify";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  adapter: vercel(),
   integrations: [solidJs()],
   experimental: {
     actions: true
